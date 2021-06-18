3bce6eac2e3e21113ae5d83ef2e7b0423c36618d
zhenyulei <yulei20080106@126.com>
2021-06-18 18:12:27 +0800

    Auto-deploy commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,8 @@ import { init } from '../scripts/gitOperate.js';
 @Injectable()
 export class AppService {
   getHello(id): string {
-    init(id);
+    const res = init(id);
+    console.log(res);
     return 'Hello World!haiai';
   }
 }
