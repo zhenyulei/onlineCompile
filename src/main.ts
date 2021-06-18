a6602cf0efc0189f83b792bb3a776d54bb97febd
zhenyulei <yulei20080106@126.com>
2021-06-18 16:28:37 +0800

    Auto-deploy commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const path = require('path');
+import * as serveStatic from 'serve-static';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    '/public',
+    serveStatic(path.resolve(__dirname, '../public'), {
+      maxAge: '1d',
+      extensions: ['html', 'jpg', 'jpeg', 'png', 'gif'],
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
