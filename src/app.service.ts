f6139a55e36ea3fe9558fe653dee73d2868ed5ad
zhenyulei <yulei20080106@126.com>
2021-07-23 17:58:58 +0800

    Auto-deploy commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { init } from '../scripts/gitOperate.js';
+import path from 'path';
+const mkdirp = require('mkdirp');
+const getDirName = require('path').dirname;
+const fs = require('fs');
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+//创建带有文件夹的文件
+function writeFile(path, contents, cb) {
+  mkdirp(getDirName(path), function (err) {
+    if (err) return cb(err);
+    fs.writeFile(path, contents, cb);
+  });
+}
+
+//生成带有回调函数
+function createViews(viewName, ViewContent) {
+  writeFile(viewName, ViewContent, function (err) {
+    if (err) throw new Error('创建views文件出错');
+  });
+}
+
 @Injectable()
 export class AppService {
   async getHello(id) {
     const res = await init(id);
+    const content = resolve(
+      '../../online-client/src/components/Welcome/Welcome.tsx',
+    );
+    const pageContent = `import * as React from "react";
+
+import logo from "./logo.svg";
+import "./Welcome.scss";
+
+const Welcome = (props: { message: string }) => {
+  return (
+    <div className="welcome-container">
+        <div>我是改编后的内容</div>
+    </div>
+  );
+};
+
+export default Welcome;
+`;
+    createViews(pageContent, content);
     return {
       success: 'res',
     };
